b59c054097919ccad7a2c091a785b1bf65ca4d74
DF <dragonflybaby2.0@gmail.com>
2023-04-16 20:12:29 +0800

    Texture Loading and Rendering

--- main.cpp
+++ main.cpp
@@ -1,48 +1,38 @@
-#include "SDL_events.h"
-#include "SDL_pixels.h"
-#include "SDL_rect.h"
-#include "SDL_surface.h"
-#include "SDL_video.h"
 #include <SDL.h>
-#include <cstdio>
-#include <iostream>
+#include <SDL_image.h>
 #include "config.h"
-#include "stuff.h"
 
 int main() {
-    SDL_Window*window = nullptr;
-    SDL_Surface*screenSurface = nullptr;
-    SDL_Surface* img = nullptr;
-    if (not init(window, screenSurface)) {
-        std::cout << "Failed to initialize\n";
-        return -1;
-    }
-    if (not loadMedia(img, screenSurface)) {
-        std::cout << "Failed to load media\n";
-        return -1;
-    }
+    SDL_Init(SDL_INIT_EVERYTHING);
+    IMG_Init(IMG_INIT_PNG);
 
-    SDL_Rect stretched;
-    stretched.x = 0;
-    stretched.y = 0;
-    stretched.w = WINDOW_WIDTH;
-    stretched.h = WINDOW_HEIGHT;
+    SDL_Window* window = SDL_CreateWindow("Hello World", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WINDOW_WIDTH, WINDOW_HEIGHT, SDL_WINDOW_SHOWN);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_BlitScaled(img, nullptr, screenSurface, &stretched);
-    SDL_UpdateWindowSurface(window);
+    SDL_Surface* surface = IMG_Load("/home/df/CLionProjects/SDLPractice/resource/image.png");
+    SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
 
-    bool quit = false;
-    SDL_Event e;
-    while (not quit) {
-        while (SDL_PollEvent(&e)) {
-            if (e.type == SDL_KEYDOWN) {
-                if (e.key.keysym.sym == SDLK_ESCAPE)
-                    quit = true;
-//                SDL_BlitScaled(img, nullptr, screenSurface, &stretched);
-//                SDL_UpdateWindowSurface(window);
+    SDL_FreeSurface(surface);
+
+    SDL_Event event;
+    bool running = true;
+    while (running) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_QUIT) {
+                running = false;
             }
         }
+        SDL_RenderClear(renderer);
+        SDL_RenderCopy(renderer, texture, NULL, NULL);
+        SDL_RenderPresent(renderer);
     }
-    close(window, img);
+
+    SDL_DestroyTexture(texture);
+    SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
+
+    IMG_Quit();
+    SDL_Quit();
+
     return 0;
 }
